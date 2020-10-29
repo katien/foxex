49e2c41ad8d718dd9e0a9577d77474163c84a219
katien <katienoland9@gmail.com>
2020-10-29 15:10:01 -0600

    handle socket io connections from server

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
+import {startListeners} from './connectionManager';
 // rest of the code remains same
 const app = express();
-const PORT = 8000;
+const PORT = 3000;
+
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
-app.listen(PORT, () => {
+let server = app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
+const io = require('socket.io')(server);
+
+startListeners(io);
+
