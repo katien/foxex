1a4e18dd69dcf87d30b2b20b8e306eed09742b9a
katien <katienoland9@gmail.com>
2020-10-30 17:19:41 -0600

    generate combined totals and return to client

--- server/orderbook/CombinedOrderBook.ts
+++ server/orderbook/CombinedOrderBook.ts
@@ -4,20 +4,37 @@ export class CombinedOrderBook {
   bittrexOrderBook?: OrderBook;
   poloniexOrderBook?: OrderBook;
 
+
   constructor(bittrexOrderBook?: OrderBook, poloniexOrderBook?: OrderBook) {
     this.bittrexOrderBook = bittrexOrderBook;
     this.poloniexOrderBook = poloniexOrderBook;
   }
 
-  get asOrderBook(): OrderBook {
-    return new OrderBook(this.bittrexOrderBook?.bid || {}, this.bittrexOrderBook?.ask || {})
+  get combinedTotals(): OrderBook {
+    return new OrderBook(this.combinedBids, this.combinedAsks)
   }
 
   get combinedBids(): Totals {
-    return this.bittrexOrderBook?.bid || {}; // + this.poloniexOrderBook.bid
+    let combined: Totals = this.poloniexOrderBook?.bid || {};
+    for (let price of Object.keys(this.bittrexOrderBook?.bid || {})) {
+      if (combined[price]) {
+        combined[price] += (this.bittrexOrderBook?.bid[price] || 0);
+      } else {
+        combined[price] = this.bittrexOrderBook?.bid[price] || 0;
+      }
+    }
+    return combined;
   }
 
   get combinedAsks(): Totals {
-    return this.bittrexOrderBook?.ask || {}; // + this.poloniexOrderBook.ask
+    let combined: Totals = this.poloniexOrderBook?.ask || {};
+    for (let price of Object.keys(this.bittrexOrderBook?.ask || {})) {
+      if (combined[price]) {
+        combined[price] += (this.bittrexOrderBook?.ask[price] || 0);
+      } else {
+        combined[price] = this.bittrexOrderBook?.ask[price] || 0;
+      }
+    }
+    return combined;
   }
 }
