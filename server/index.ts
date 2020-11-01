c79fc59acae8e57de5fc4e6d582a270de7e5d5ee
katien <katienoland9@gmail.com>
2020-11-01 13:30:31 -0700

    heroku

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 let server = app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
-app.use(express.static( '/client/dist', {index: "index.html"}));
+app.use(express.static( 'client/dist', {index: "index.html"}));
 
 // register socketio with http server
 const io = require('socket.io')(server);
