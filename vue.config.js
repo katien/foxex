54ae30316341f3443e1a4e81b21292307b768f85
katien <katienoland9+1@gmail.com>
2020-11-03 21:48:01 -0700

    trying to serve custom favicon

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 /**
  * These configurations point vue-cli-service at the ./client directory
  * */
 module.exports = {
-  outputDir:'./client/dist',
+  outputDir: './client/dist',
   chainWebpack: config => {
     config
       .plugin('fork-ts-checker')
@@ -10,10 +12,29 @@ module.exports = {
         args[0].tsconfig = './client/tsconfig.json';
         return args;
       });
+
+    config.plugin("html")
+      .tap(args => {
+        args[0].template = "./client/public/index.html";
+        return args;
+      })
   },
   configureWebpack: {
     entry: {
       app: './client/src/main.ts'
-    }
+    },
+
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          {
+            from: './client/public', to: './',
+            globOptions: {
+              ignore: ['index.html'],
+            },
+          },
+        ]
+      }),
+    ],
   }
 };
