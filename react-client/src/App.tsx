5b254ce9187a9cf8dcbeaf92185b13a9736efcd5
katien <katienoland9+1@gmail.com>
2020-11-09 20:27:20 -0700

    create enum to represent CurrencyPairs, add support for changing subscribed pair

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,47 +1,60 @@
-import React, {useEffect, useState} from 'react';
+import React, {SyntheticEvent, useEffect, useState} from 'react';
 import io from 'socket.io-client';
 import './App.css';
 import {OrderBook} from "./types/OrderBook";
 import OrderTable from "./orderbook/OrderTable";
+import {CurrencyPair} from "./types/CurrencyPair";
 
 interface AppState {
-  currencyPair: string,
+  currencyPair: CurrencyPair,
   orderBook?: OrderBook
 }
 
 class App extends React.Component<{}, AppState> {
-  socket: SocketIOClient.Socket
+  socket: SocketIOClient.Socket // not reactive state - safe to store as a class field
 
+  /**
+   * initialize state and socket connection, set orderBookLoaded event callback
+   * */
   constructor(props: {}) {
     super(props);
-    this.state = {
-      currencyPair: "BTC_ETH"
-    };
-
+    this.state = {currencyPair: CurrencyPair.BTC_ETH};
     this.socket = process.env.NODE_ENV === 'development' ? io("localhost:3000") : io();
     this.socket.on("orderBookLoaded", this.orderBookLoaded);
   }
 
+  /**
+   * subscribe to default currencyPair
+   * done here instead of constructor so orderBookLoaded doesn't invoke setState before component is rendered
+   * */
   componentDidMount(): void {
-    // subscribe in componentDidMount instead of constructor so that orderBookLoaded doesn't call setState before the component is rendered
     this.socket.emit("subscribe", this.state.currencyPair);
   }
 
+  /**
+   * Callbacks
+   * use public class field syntax so that this is bound to the class instance and setState is available
+   * */
   orderBookLoaded = (orderBook: OrderBook) => this.setState({orderBook});
+  onChange = (e: React.FormEvent<HTMLSelectElement>): void => {
+    const currencyPair: CurrencyPair | undefined = (CurrencyPair)[e.currentTarget.value as CurrencyPair];
+    this.setState({currencyPair});
+    this.socket.emit("subscribe", currencyPair);
+  };
 
   render() {
+    let options = Object.keys(CurrencyPair).map(pair => <option key={pair}>{pair}</option>);
     return (
       <div id="app">
         <h1>Foxex</h1>
-        {/*<select value={this.state.currencyPair}>*/}
-        {/*  <option>BTC_ETH</option>*/}
-        {/*</select>*/}
+        <select value={this.state.currencyPair} onChange={this.onChange}>
+          {options}
+        </select>
         <OrderTable title="ask" entries={this.state.orderBook?.ask}/>
         <OrderTable title="bid" entries={this.state.orderBook?.bid}/>
       </div>
     );
   }
-
 }
 
 export default App;
---
+++ react-client/src/types/CurrencyPair.ts
@@ -0,0 +1,4 @@
+export enum CurrencyPair {
+  BTC_ETH = "BTC_ETH",
+  BTC_DOGE = "BTC_DOGE"
+}
